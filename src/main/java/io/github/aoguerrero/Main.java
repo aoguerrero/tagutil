80047b8a705295da93a6210a2ee82b09ce31a6a3
Andres <andres.guerrero@mayasoft.com.co>
2022-02-08 12:02:46 -0500

    Removed logback

--- src/main/java/io/github/aoguerrero/Main.java
+++ src/main/java/io/github/aoguerrero/Main.java
@@ -1,22 +1,17 @@
 package io.github.aoguerrero;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Main {
-
-	private static Logger logger = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) throws Exception {
 		try {
 			Params params = new Params(args);
-			logger.info("Processing: " + params.getFile().getCanonicalPath());
+			System.out.println("Processing: " + params.getFile().getCanonicalPath());
 			SongInfo songInfo = new SongInfo(params.getFile());
-			logger.info("Song information from the file name " + songInfo);
+			System.out.println("Song information from the file name " + songInfo);
 			TagWriter.writeTag(params.getFile(), songInfo);
 			FileNameUtils.rename(params.getFile(), songInfo);
 		} catch (TagUtilException tue) {
-			logger.error("Error: " + tue.getMessage());
+			System.out.println("Error: " + tue.getMessage());
 		}
 	}
 }
